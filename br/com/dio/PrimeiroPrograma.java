33461270666cad0614a985c8a2c4ebc6cbe81edc
alexa <alexandresouza9@gmail.com>
2021-11-16 14:11:36 -0300

    first commit

---
+++ br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,19 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		System.out.println(gato);
+		
+		// TODO Auto-generated method stub
+		/*int a = 2;
+		int b = 3;
+		System.out.println(" Hellor mundo " + (a + b) );
+		*/
+	}
+
+}
